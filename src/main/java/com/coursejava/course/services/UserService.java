786c89ca307c64b6d0e33d64ab1d98733ab5d35a
H3nrikito <jhowhp@yahoo.com.br>
2021-10-23 12:04:18 -0300

    Exception handling - update

--- src/main/java/com/coursejava/course/services/UserService.java
+++ src/main/java/com/coursejava/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.coursejava.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -13,53 +15,52 @@ import com.coursejava.course.repositories.UserRepository;
 import com.coursejava.course.services.exceptions.DatabaseException;
 import com.coursejava.course.services.exceptions.ResourceNotFoundException;
 
-@Service //declarando a classe como um componente do framework spring boot
+@Service // declarando a classe como um componente do framework spring boot
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); //senao encontrar o usuario, lança a exceção
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // senao encontrar o usuario, lança a exceção
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
-	
+
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-		}
-		catch (EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-		
+
 	}
-	
-	public User update (Long id, User obj) {
-	 User entity = repository.getOne(id); //vai instanciar o user - melhor q o findById(vai buscar o id) - mais eficiente
-	 updateData(entity, obj); //atualizar o entity com o obj
-	 return repository.save(entity);
-	 	 
+
+	public User update(Long id, User obj) {
+		try {
+			User entity = repository.getOne(id); // vai instanciar o user - melhor q o findById(vai buscar o id) - mais eficiente
+			updateData(entity, obj); // atualizar o entity com o obj
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateData(User entity, User obj) {
-		//atualizar os dados do entity com base no q chegou no obj
-		entity.setName(obj.getName()); 
+		// atualizar os dados do entity com base no q chegou no obj
+		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-	
-	
-	
+
 }
